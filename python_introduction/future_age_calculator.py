e25fc8b4ccee873aa8502f12d2ec92ad74818f78
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-05-22 23:55:21 +0100

    Commit message 3

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+user_age = int(input("How old are you"))
+current_year = 2023
+future_year = 2050
+
+estimated_age = (future_year - current_year) + user_age
+print ("In", future_year, "you will be", estimated_age, "years old.")
