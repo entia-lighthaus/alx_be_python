ef652cfd2ba4822a69764327c158f093e5004d94
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-05-23 00:30:55 +0100

    Commit message 4

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,3 +8,4 @@ projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print ("Your monthly savings are", monthly_savings)
 print ("Projected savings after one year, with interest, is:", projected_savings )
+
