8c88bb0c232f7e83074e14b6b36d4e0a34442e50
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-06-06 19:27:17 +0100

    Date Time Practical Work

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current Date and Time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+def calculate_future_date(days):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+    print("Future Date:", future_date.strftime("%Y-%m-%d"))
+
+def main():
+    # Part 1
+    display_current_datetime()
+
+    # Part 2
+    try:
+        days = int(input("Enter the number of days to add: "))
+        calculate_future_date(days)
+    except ValueError:
+        print("Please enter a valid integer for the number of days.")
+
+if __name__ == "__main__":
+    main()
