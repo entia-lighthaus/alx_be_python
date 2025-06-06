5a51953356fc85856cdcceeb7de5ce7e96bfe689
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-06-06 19:17:04 +0100

    forth commit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -18,7 +18,7 @@ def main():
             continue
 
         if choice == 1:
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} added to shopping list.")
 
