1ff54dd7b46b2a72d7960e8f348b990d40e1614e
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-06-09 18:34:24 +0100

    Add simple calculator and unit tests

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,33 @@
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Create a SimpleCalculator instance for testing."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -7), -12)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
+        self.assertEqual(self.calc.subtract(-3, -6), 3)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-2, 5), -10)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-15, 3), -5)
+        self.assertIsNone(self.calc.divide(10, 0))  # division by zero
+        self.assertEqual(self.calc.divide(7, 2), 3.5)
+
+if __name__ == '__main__':
+    unittest.main()
